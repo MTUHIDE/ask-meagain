2c8a9bf14be56f7a88de9cbcb38be66511d67264
Max Sexauer <msexauer@arbor.net>
2018-06-03 23:24:57 -0400

    Bunch of changes to start serving files in this application. I'll quickly run through the important ones.
    -Docker file changes: The two lines allow for static path in Flask. This
    took me 2 hours to figure out :(
    
    -static/assets/: Now css and images are hosted here. I just copied over
    one image file for testing so far, but moved the css over in bulk
    because why not?
    
    -Templates: html gets hosted from here. I've moved over one page for
    testing so far and it works. So that it ran without a ton of errors for
    a first, usability test, I commented pretty much everything out.

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM tiangolo/uwsgi-nginx-flask:python3.6
 
+ENV STATIC_URL /static
+ENV STATIC_PATH /app/app/static
+
 COPY ./app /app
--- app/app/main.py
+++ app/app/main.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World from HIDE's new solution"
+
+@app.route("/menu/")
+def menu():
+    return render_template('menu.html')
+
 
 if __name__ == "__main__":
     # Only for debugging while developing
