c702d8c8e024283cbc414ee5b7c7faf879ef9017
Max Sexauer <msexauer@arbor.net>
2018-09-19 16:39:35 -0400

    Enabled use of rebuild flag while docker image is still running

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # To be run on host machine to start up docker container
+# IMPORTANT NOTES BELOW
+#   It's best to run this from the source directory. Not doing this can cause
+#       problems
+#   Running with -r will stop your docker instance, then rebuild and start it
 
 # Define variables
 imageName="script-build"
@@ -9,8 +13,14 @@ check=`docker image ls | grep $imageName | cut -d' ' -f27`
 if [ $check ]
 then
   echo "Docker build already exists"
-  if [[ $1 == "-r" ]] # Checking for "-r flag for the purposes of rebuilding the project"
+  if [[ $1 == "-r" ]] # Checking for "-r" flag - rebuild the project
   then
+    echo "Checking to make sure docker is stopped"
+    if [ `docker ps -q` ]
+    then
+        echo "Docker is running, stopping docker now"
+        ./stop.sh
+    fi
     echo "Rebuilding docker image"
     docker rmi -f $(docker image ls -q)
     docker build -t ask:$imageName .
