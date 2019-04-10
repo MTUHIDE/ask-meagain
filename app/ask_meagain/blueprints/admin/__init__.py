0e0ce31d3a7a539293140db947438762ff117536
FoamCactus <cahill@mtu.edu>
2019-04-10 17:45:51 -0400

    added macros and started on alter db stuff

--- app/ask_meagain/blueprints/admin/__init__.py
+++ app/ask_meagain/blueprints/admin/__init__.py
@@ -1,15 +1,35 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request
 from ask_meagain.models import TestResponses, TestChoices, TestQuestions
+from ask_meagain.blueprints.admin.adminForms import newQuestionForm
+from ask_meagain import db
 
 admin = Blueprint('admin',__name__,template_folder='templates')
 
 @admin.route("/alterQuestion/")
 def alterQuestion():
-    return render_template('alterQuestion.html')
+    questions = TestQuestions.query.filter_by(active = True).all()
+    return render_template('alterQuestion.html',questions = questions)
 
-@admin.route("/createQuestion/")
+@admin.route("/createQuestion/",methods=['GET','POST'])
 def createQuestion():
     form = newQuestionForm()
+    if(request.method == 'POST'):
+        question = TestQuestions()
+        question.questionText = request.form['question'] 
+        db.session.add(question)
+        db.session.commit()
+
+        
+        optionOne = TestChoices(qid=question.id,choiceText=request.form['optionOne'])
+        optionTwo = TestChoices(qid=question.id,choiceText=request.form['optionTwo'])
+        optionThree = TestChoices(qid=question.id,choiceText=request.form['optionThree'])
+        optionFour = TestChoices(qid=question.id,choiceText=request.form['optionFour'])
+
+        db.session.add(optionOne)
+        db.session.add(optionTwo)
+        db.session.add(optionThree)
+        db.session.add(optionFour)
+        db.session.commit()
     return render_template("create.html",form=form)
 
 @admin.route("/ResultsPage/")
@@ -19,10 +39,11 @@ def resultsPage():
 
 @admin.route("/QuestionResultsPage/<id>")
 def questionResultPage(id):
-    data = TestResponses.query.filter_by(qid = id).first() 
-    return render_template('questionResults.html')
+    data = TestQuestions.query.filter_by(id = id).first() 
+    return render_template('questionResults.html',question = data)
 
 @admin.route("/questionData/<id>")
 def questionData(id):
     data = TestResponses.query.filter_by(qid = id).all()
+    printf(jsonify(data))
     return jsonify(data)
--- app/ask_meagain/models.py
+++ app/ask_meagain/models.py
@@ -7,12 +7,18 @@ class TestQuestions(db.Model):
     active = db.Column(db.Boolean, default=True)
     choices = db.relationship('TestChoices', backref='testQ')
     responces = db.relationship('TestResponses', backref='testR')
+    
+    def __str__(self):
+        return self.questionText
 
 class TestChoices(db.Model):
     __tablename__ = 'testC'
     qid = db.Column(db.Integer, db.ForeignKey('testQ.id'))
     choiceText = db.Column(db.Text)
     choiceId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    def __str__(self):
+        return self.choiceText
 
 class TestResponses(db.Model):
     __tablename__ = 'testR'
@@ -21,3 +27,6 @@ class TestResponses(db.Model):
     responce = db.Column(db.Integer)
     dt = db.Column(db.DateTime)
     studentId = db.Column(db.Integer)
+
+    def __str__(self):
+        return self.responce
