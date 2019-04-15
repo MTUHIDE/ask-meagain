94e68c2abff6c97f133ecc9f4767968e756bd05f
FoamCactus <cahill@mtu.edu>
2019-04-14 22:50:00 -0400

    made it easier to check if changes saved.

--- app/ask_meagain/blueprints/survey/__init__.py
+++ app/ask_meagain/blueprints/survey/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint,render_template,request, flash, redirect, url_for
 from ask_meagain.blueprints.survey.surveyQuestions import QuestionForm
+from ask_meagain.models import TestQuestions
 survey = Blueprint('survey',__name__,template_folder='templates')
 
 @survey.route("/")
@@ -18,6 +19,13 @@ def surveyQuestion():
             flash('ERR')
             return render_template('survey.html', form = form)
         else:
+            print(request.form)
             return redirect(url_for('survey.thankYouPage'))
     elif request.method == 'GET':
+        question = TestQuestions.query.order_by(TestQuestions.id.desc()).first()
+        form.question.default = question.questionText
+        form.answer1.label.text = question.choices[0].choiceText 
+        form.answer2.label.text = question.choices[1].choiceText 
+        form.answer3.label.text = question.choices[2].choiceText 
+        form.answer4.label.text = question.choices[3].choiceText 
         return render_template('survey.html', form = form)
