5368241e7138d170ddfae5a277519330a215db74
Raghav Puri <raghavpu@mtu.edu>
2020-01-29 18:18:05 -0500

    django replacement
    replacing with django

--- app/blueprints/admin/adminForms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-
-class newQuestionForm(FlaskForm):
-    question = StringField('Question')
-    optionOne = StringField('optionOne')
-    optionTwo= StringField('optionTwo')
-    optionThree = StringField('optionThree')
-    optionFour = StringField('optionFour')
