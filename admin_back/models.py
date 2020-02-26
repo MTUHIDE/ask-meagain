83040c7c8b5acb74111898aa1ff4e8c1f3429775
Scott <scottcorbat@yahoo.com>
2020-02-26 17:38:06 -0500

    Beginning database design

--- admin_back/models.py
+++ admin_back/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Survey(models.Model):
+    title = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.survey + ": " + self.text
+
+
+class Choice(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.survey + ": " + self.question + ": " + self.text
+
+
+class Taken(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    student = models.CharField(max_length=20)
