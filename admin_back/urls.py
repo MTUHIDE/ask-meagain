31c129594337414d4db98af695b75b65e5a5e377
jacob <jacobl4476@gmail.com>
2020-02-26 18:47:20 -0600

    home html page

--- admin_back/urls.py
+++ admin_back/urls.py
@@ -7,6 +7,7 @@ app_name = 'admin_back'
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.loginauth, name='loginauth'),
+    path('home', views.home, name='home'),
     path('create_question/<int:survey_id>/', views.create_question, name='create_question'),
 
 ]
--- admin_back/views.py
+++ admin_back/views.py
@@ -6,8 +6,8 @@ from django.template import loader
 def index(request):
     #template = loader.get_template('admin/index.html')
     context = {}
-
     return render(request, 'admin_back/index.html', context)
+
 def loginauth(request):
     context = {}
     return render(request, 'admin_back/loginauth.html', context)
@@ -17,3 +17,10 @@ def create_question(request, survey_id):
     context = {}
     template = loader.get_template('admin_back/create_question.html')
     return HttpResponse(template.render(context, request))
+
+def home(request):
+    context = {}
+    template = loader.get_template('admin_back/home.html')
+    return render(request, 'admin_back/home.html', context)
+
+
