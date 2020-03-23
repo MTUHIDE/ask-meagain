5db9c59573363a0c7cd9c5c70dbfc9e417cba345
Scott <scottcorbat@yahoo.com>
2020-03-23 16:45:03 -0400

    documentation for django forms

--- admin_back/forms.py
+++ admin_back/forms.py
@@ -1,5 +1,20 @@
 from django import forms
 
+"""
+    Creating a question requires an existing survey, to add one to your database do the following:
+    In your terminal run 'python manage.py shell'
+    
+    >>> from admin_back.models import Survey, Question, Choice
+    >>> survey = Survey(title="testSurvey")
+    >>> survey.save()
+"""
+
+"""
+    IMPORTANT
+    Variable names in forms must match the names given in the HTML code, i.e.
+    questiontext is the same variable name as the <input> tag in the HTML code has in its name element
+"""
+
 
 class QuestionForm(forms.Form):
     questiontext = forms.CharField(label='Question text', max_length=200)
