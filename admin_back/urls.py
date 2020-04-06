ea405c97fce80d98f2e347ec5fe04bd7c74ae662
Tejal <tbohara@mtu.edu>
2020-04-06 16:16:53 -0400

    demo login page file structure for putting staticfiles

--- admin_back/urls.py
+++ admin_back/urls.py
@@ -7,8 +7,8 @@ app_name = 'admin_back'
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.loginauth, name='loginauth'),
-    path('home', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('create_question/<int:survey_id>/', views.create_question, name='create_question'),
     path('form_test', views.form_test, name='form_test'),
-
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
